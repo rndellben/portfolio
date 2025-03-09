2d7ca9ce8c4f2de144fe927b6eea9e435ff64443
rndellben <rbcalilung@gmail.com>
2025-03-09 12:24:43 +0800

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export"
+    output: "export", // Enables static export
+    images: {
+      unoptimized: true, // Needed because GitHub Pages does not support Next.js image optimization
+    },
+    basePath: "/portfolio", // Replace with your GitHub repository name
+    assetPrefix: "/portfolio/", // Replace with your GitHub repository name
 };
 
 export default nextConfig;
